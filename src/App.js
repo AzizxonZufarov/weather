426388c9ce757ab7b2a112bb4cd7acb2ca951b24
AzizxonZufarov <bionorica2015@mail.ru>
2022-03-13 18:52:24 +0500

    w

--- src/App.js
+++ src/App.js
@@ -22,12 +22,14 @@ if(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`);
     const data = await api_url.json();
     var sunset = data.sys.sunset;
+    const kelvin = Math.round(data.main.temp);
+    const celsius = Math.round(kelvin - 273,15);
     var date = new Date();
     date.setTime(sunset);
     var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
     this.setState({
-      temp: data.main.temp,
+      temp: celsius,
       city:data.name,
       country:data.sys.country,
       sunset:sunset_date,
